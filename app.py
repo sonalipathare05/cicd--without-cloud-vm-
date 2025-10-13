716c40162a1f5355da4970a207478d7fb9879771
Your Name <you@example.com>
2025-10-13 22:21:44 +0530

    Initial commit for CI/CD project

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+# Step 1: Create a Flask application
+app = Flask(__name__)
+
+# Step 2: Define a route (the URL the user visits)
+@app.route('/')
+def home():
+    return "Hello from my first CI/CD project!"
+
+# Step 3: Run the app
+if __name__ == '__main__':
+    # host='0.0.0.0' means it’s accessible from any machine (good for Docker)
+    app.run(host='0.0.0.0', port=5000)
